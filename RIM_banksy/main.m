b42d6e4b43d014387ba58158c410ab33729dd188
banksycjt <juntongcao@outlook.com>
2025-04-15 12:14:52 +0800

    202504151214

--- RIM_banksy/main.m
+++ RIM_banksy/main.m
@@ -1,8 +1,8 @@
 %% initializtion
 
-addpath(genpath('Z:\caojuntong\code\RIM\RIM_package'))
+addpath(genpath('E:\banksycjt_git\non_linear_RIM\non_linear_RIM\RIM_mpackage'))
 
-RawPicturePath = 'Z:\caojuntong\code\RIM\data\collect_images_32bit.tif';
+RawPicturePath = 'E:\OneDrive - stu.ecnu.edu.cn\collected_images2.tiff';
 tiffFiles = dir(RawPicturePath); % tiffFiles : struct of information of the tiffs
 img = mLoadImg(RawPicturePath, [], [], [], 0);  % image in cells
 
@@ -11,15 +11,16 @@ cutFreq = 0.2; % cutoff frequency
 varExpFactorFieldValue = 2; % Image expansion factor, physical size of pixels will be divided by this factor.
 curSize = varExpFactorFieldValue*size(img{1}); % expand image
 varShowImages = 1; % 1 represent show all the image
-varNumItFieldValue = 100;% varNumItFieldValue : number of iteration
+varNumItFieldValue = 20;% varNumItFieldValue : number of iteration
 varRegularisationParameter = 0.15; %L2 parmeter
 
-varPathPsfEmission = 'Z:\caojuntong\data\PSF\';
-varFilePsfEmission = 'PSF256_520nm.tif' ;
+varPathPsfEmission = 'E:\banksycjt_git\non_linear_RIM\non_linear_RIM\line_pair\';
+varFilePsfEmission = 'PSF512_520nm.tif' ;
 varExpFactorPSFFieldValue = 1; % psf expansion factor, physical size of pixels will be divided by this factor.
-varPathPsfExcitation = 'Z:\caojuntong\data\PSF\';
-varFilePsfExcitation = 'PSF256_405nm.tif' ;
+varPathPsfExcitation = 'E:\banksycjt_git\non_linear_RIM\non_linear_RIM\line_pair\';
+varFilePsfExcitation = 'PSF512_488nm.tif' ;
 varWienerParamFieldValue = 0.15; % varWienerParamFieldValue : Wiener filtering parameter
+
 
 %% otfsCalculation
 
